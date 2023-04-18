8a6760c7259f6637b0c05b6a9f92e4da66ada307
Ricardo M <rmoreiradev@gmail.com>
2023-04-18 23:39:15 +0100

    added 5th

---
+++ next05/src/app/api/echo/route.ts
@@ -0,0 +1,9 @@
+import { NextResponse } from "next/server";
+
+export async function GET(request: Request) {
+  const { searchParams } = new URL(request.url);
+
+  const params = Object.fromEntries(searchParams.entries());
+
+  return NextResponse.json(params);
+}
