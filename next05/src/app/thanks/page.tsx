3bab384e0a1637079a0b211459a8bf1ed26ea7ef
Ricardo M <rmoreiradev@gmail.com>
2023-04-25 22:25:53 +0100

    updated 5th

---
+++ next05/src/app/thanks/page.tsx
@@ -0,0 +1,7 @@
+export default function Thanks() {
+  return (
+    <main>
+      <h1 className="text-3xl grid place-content-center min-h-screen">Thank you for your feedback!</h1>
+    </main>
+  );
+}
